021ce8c4561b4e29593441064547197518a96ea2
Novian Andika <45744788+Novian227@users.noreply.github.com>
2020-05-11 14:15:17 +0700

    add hard delete function

--- application/views/barang/list_barang.php
+++ application/views/barang/list_barang.php
@@ -17,7 +17,6 @@
 
 <script type="text/javascript">
 
-
     function loadKonten(url) {
         $.ajax(url, {
             type: 'GET',
@@ -34,12 +33,33 @@
         })
     }
 
-function reload_event() {
-    $('.linkEditBarang').on('click', function () {
-        var hashClean = this.hash.replace('#', '');
-        loadMenu('<?= base_url('barang/form_edit/')?>' + hashClean);
-    });
-}
+    function reload_event() {
+        $('.linkEditBarang').on('click', function () {
+            var hashClean = this.hash.replace('#', '');
+            loadMenu('<?= base_url('barang/form_edit/')?>' + hashClean);
+        });
+
+        $('.linkHapusBarang').on('click', function () {
+            var hashClean = this.hash.replace('#', '');
+            hapusData(hashClean);
+        });
+    }
+
+    function hapusData(id_barang) {
+        var url = 'http://localhost/backend_inventory_4133/barang/delete_data?id_barang='+id_barang;
+
+        $.ajax(url, {
+            type: 'GET',
+            success: function (data, status, xhr) {
+                var objData = JSON.parse(data);
+                alert(objData ['pesan'] );
+                loadKonten('http://localhost/backend_inventory_4133/barang/list_barang');
+            },
+            error: function (jqXHR, textStatus, errorMsg) {
+                alert('Error: ' +errorMsg);
+            }
+        })
+    }
 
     loadKonten('http://localhost/backend_inventory_4133/barang/list_barang');
 </script>
